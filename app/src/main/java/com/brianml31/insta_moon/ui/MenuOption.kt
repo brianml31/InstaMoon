1bbf15d6c62abda8dcf3524c6b79a379364e662b
Yiğit <40565628+tryigit@users.noreply.github.com>
2025-05-04 18:52:35 +0300

    Update MenuOption.kt

--- app/src/main/java/com/brianml31/insta_moon/ui/MenuOption.kt
+++ app/src/main/java/com/brianml31/insta_moon/ui/MenuOption.kt
@@ -1,7 +1,11 @@
 package com.brianml31.insta_moon.ui
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class MenuOption(
     val id: Int,
     val icon: String,
     val text: String
-)
+) : Parcelable
