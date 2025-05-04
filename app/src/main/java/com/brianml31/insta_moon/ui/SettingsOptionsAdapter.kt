ca69413383f01816c563a183678727fb66bba741
Yiğit <40565628+tryigit@users.noreply.github.com>
2025-05-04 19:05:05 +0300

    Update SettingsOptionsAdapter.kt

--- app/src/main/java/com/brianml31/insta_moon/ui/SettingsOptionsAdapter.kt
+++ app/src/main/java/com/brianml31/insta_moon/ui/SettingsOptionsAdapter.kt
@@ -5,7 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.brianml31.insta_moon.R // R dosyasını import etmeyi unutma
+import com.brianml31.insta_moon.R
 import com.google.android.material.switchmaterial.SwitchMaterial
 
 class SettingsOptionsAdapter(
