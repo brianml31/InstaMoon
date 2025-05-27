dd9f4ad74c655804fef9b890d633bd161f60b796
BrianML ✧ <s20120168@alumnos.itsur.edu.mx>
2025-05-27 09:03:59 -0600

    InstaMoon V5
    Update version code, version name and themes.xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.brianml31.instamoon"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "4.0"
+        versionCode = 5
+        versionName = "5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
